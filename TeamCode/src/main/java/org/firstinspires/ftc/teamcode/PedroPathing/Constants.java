3619331b0e6dd586aebf7cf74e9e7b2209d86e3f
RithekShankar <145854041+RithekShankar@users.noreply.github.com>
2025-10-04 10:19:26 -0400

    tuned

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PedroPathing/Constants.java
@@ -1,20 +1,31 @@
 package org.firstinspires.ftc.teamcode.PedroPathing;
 
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
 import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.ftc.localization.Encoder;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.pedropathing.ftc.localization.constants.TwoWheelConstants;
 import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
-    public static FollowerConstants followerConstants = new FollowerConstants().mass(16.65);
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(7.9)
+            .forwardZeroPowerAcceleration(-41.54307)
+            .lateralZeroPowerAcceleration(-55)
+            .translationalPIDFCoefficients(new PIDFCoefficients(.6,.0001,.055,0))
+            .headingPIDFCoefficients(new PIDFCoefficients(5,6,.2,.01))
+            .centripetalScaling(.0009)
+            ;
 
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1)
+            ;
 
 
     public static MecanumConstants driveConstants = new MecanumConstants()
@@ -26,32 +37,29 @@ public class Constants {
             .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-            .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE)
-            ;
-    public static TwoWheelConstants localizerConstants = new TwoWheelConstants()
-            .forwardEncoder_HardwareMapName("frontLeft")
-            .forwardEncoderDirection(1)
-            .strafeEncoderDirection(-1)
-            .strafeEncoder_HardwareMapName("backLeft")
-            .IMU_HardwareMapName("imu")
-            .IMU_Orientation(
-                    new RevHubOrientationOnRobot(
-                            RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
-                            RevHubOrientationOnRobot.UsbFacingDirection.UP
-                    )
-            )
-            .strafePodX(-2.5)
-            .forwardPodY(6.89)
-            .forwardTicksToInches(.002980516956640465)
-            .strafeTicksToInches(.002965149766084306)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(58.18826629)
+            .yVelocity(48.0511835)
+
 
             ;
+
+    public static PinpointConstants pinpointConstants = new PinpointConstants()
+            .strafePodX(-4)
+            .forwardPodY(4.75)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD)
+            .hardwareMapName("pinpoint")
+            ;
+
+
 
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
                 .mecanumDrivetrain(driveConstants)
-                .twoWheelLocalizer(localizerConstants)
+                .pinpointLocalizer(pinpointConstants)
+
                 .build();
 
     }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UtilityOpModes/AutoRedPoseFinder.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode.UtilityOpModes;
+
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.PedroPathing.Constants;
+
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.extensions.pedro.FollowPath;
+import dev.nextftc.extensions.pedro.PedroComponent;
+import dev.nextftc.ftc.NextFTCOpMode;
+import static dev.nextftc.extensions.pedro.PedroComponent.follower;
+
+@TeleOp
+public class AutoRedPoseFinder extends NextFTCOpMode {
+    public AutoRedPoseFinder(){
+        addComponents(
+                new PedroComponent(Constants::createFollower)
+        );
+
+    }
+    @Override
+    public void onInit(){
+        follower().setPose(new Pose(0,0,3*Math.PI/2));
+
+    }
+    @Override
+    public void onUpdate() {
+        telemetry.addData("Pose",follower().getPose());
+        telemetry.update();
+    }
+}
