de7f70d93f3d1da05724e0217cab7f9bb034a1d5
Yozimo <manuellinkinfo@gmail.com>
2025-11-13 19:53:58 -0500

    Shooter code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterAngle.java
@@ -0,0 +1,106 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.pedropathing.control.Controller;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import org.firstinspires.ftc.teamcode.PedroPathing.Constants;
+
+import dev.nextftc.control.ControlSystem;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.components.BindingsComponent;
+import dev.nextftc.core.components.SubsystemComponent;
+import dev.nextftc.extensions.pedro.PedroComponent;
+import dev.nextftc.ftc.GamepadEx;
+import dev.nextftc.ftc.Gamepads;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.hardware.impl.MotorEx;
+
+
+import static dev.nextftc.extensions.pedro.PedroComponent.follower;
+
+
+@TeleOp
+public class ShooterAngle extends NextFTCOpMode {
+    ShooterAngleSub shooterAngle = ShooterAngleSub.INSTANCE;
+    private Limelight3A limelight3A;
+    Command path1Commmand;
+    private MotorEx motorEx;
+    private ControlSystem controlSystem;
+    private GamepadEx gamepad1;
+
+    private double distance;
+    private double velocity;
+    private double angle;
+
+
+    public ShooterAngle(){
+        addComponents(
+                new PedroComponent(Constants::createFollower),
+                new SubsystemComponent(ShooterAngleSub.INSTANCE), BindingsComponent.INSTANCE
+        );
+
+
+
+
+
+
+    }
+    @Override
+    public void onInit(){
+        follower().setPose(new Pose(0,0,3*Math.PI/2));
+        limelight3A = hardwareMap.get(Limelight3A.class, "limeLight");
+        limelight3A.start();
+        limelight3A.pipelineSwitch(0);
+        limelight3A.start();
+
+
+
+
+
+
+
+    }
+    @Override
+    public void onStartButtonPressed(){
+        Gamepads.gamepad1().a().whenBecomesTrue(ShooterAngleSub.INSTANCE ::setVelo);
+    }
+
+
+
+    @Override
+    public void onUpdate(){
+
+        LLResult result = limelight3A.getLatestResult();
+        if(result != null && result.isValid()){
+                Pose3D botPose = result.getBotpose_MT2();
+                distance = getDistance(result.getTa());
+
+
+                double x = botPose.getPosition().x;
+                double y = botPose.getPosition().y;
+                telemetry.addData("MT2 Location:", "(" + x + ", " + y + ")");
+                telemetry.addData("Distance ", distance);
+                telemetry.addData("ty", result.getTy());
+                telemetry.update();
+
+            }
+            else{
+                telemetry.addData("Nothing found",result.isValid());
+            }
+        telemetry.addData("Nothing found",result.isValid());
+            telemetry.update();
+
+    }
+
+    private double getDistance(double ta){
+        double scale = 38665.95;
+        double distance = (scale/ta);
+        return distance;
+    }
+    
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterAngleSub.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import dev.nextftc.control.ControlSystem;
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.hardware.impl.MotorEx;
+
+@Configurable
+public class ShooterAngleSub implements Subsystem {
+    public static final ShooterAngleSub INSTANCE = new ShooterAngleSub();
+    private MotorEx motorEx;
+    private ControlSystem controlSystem;
+
+    private double kP = 0.005;
+    private double kI;
+    private double kD;
+    private double curPose;
+    private double updatePose;
+
+    public ShooterAngleSub(){
+        motorEx = new MotorEx("shooterMotor");
+        controlSystem = ControlSystem.builder().velPid(kP,kI,0).build();
+        motorEx.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorEx.setCurrentPosition(curPose);
+        motorEx.setPower(0);
+        motorEx.reverse();
+        motorEx.zero();
+        curPose =0;
+        updatePose =0;
+
+    }
+    public void setVelo(){
+        motorEx.setPower(1);
+
+    }
+}
