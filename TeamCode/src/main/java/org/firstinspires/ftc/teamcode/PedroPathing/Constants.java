b1909ae29b62051db8178640fbbfdd3976222932
Yozimo <manuellinkinfo@gmail.com>
2025-12-09 19:03:32 -0500

    Updated Constants for bot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PedroPathing/Constants.java
@@ -17,8 +17,8 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(7.9)
-            .forwardZeroPowerAcceleration(-41.54307)
-            .lateralZeroPowerAcceleration(-55)
+            .forwardZeroPowerAcceleration(-33.6250884677704)
+            .lateralZeroPowerAcceleration(-63.52709172579524)
             .translationalPIDFCoefficients(new PIDFCoefficients(.6,.0001,.055,0))
             .headingPIDFCoefficients(new PIDFCoefficients(5,6,.2,.01))
             .centripetalScaling(.0009)
@@ -38,15 +38,15 @@ public class Constants {
             .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
             .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
-            .xVelocity(58.18826629)
-            .yVelocity(48.0511835)
+            .xVelocity( 77.03174837367742)
+            .yVelocity(62.43715877983514)
 
 
             ;
 
     public static PinpointConstants pinpointConstants = new PinpointConstants()
-            .strafePodX(-4)
-            .forwardPodY(4.75)
+            .strafePodX(-3.25)
+            .forwardPodY(-5.575)
             .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.REVERSED)
             .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
             .hardwareMapName("pinpoint")
